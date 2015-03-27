922a2ea3ed494dd2c5739debf4baf14c25e4efbe
Cade Ward <cadebward@gmail.com>
2015-03-27 16:40:32 -0600

    create component for ship cells in list view

---
+++ ShipCell.js
@@ -0,0 +1,44 @@
+/**
+ * ShipCell
+ * 
+ * React Native
+ * https://github.com/facebook/react-native
+ */
+
+'use strict';
+
+var React = require('react-native');
+var {
+  View,
+  TouchableHighlight,
+  StyleSheet,
+  Text
+} = React;
+
+var ShipCell = React.createClass({
+  render: function() {
+    return (
+      <View>
+        <TouchableHighlight onPress={this.props.onPress}>
+          <View style={styles.container}>
+            <Text>{ this.props.ship.name }</Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+    borderBottomWidth: 1,
+    borderColor: '#eeeeee',
+    paddingTop: 20,
+    paddingRight: 20,
+    paddingBottom: 20,
+  },
+});
+
+module.exports = ShipCell;
